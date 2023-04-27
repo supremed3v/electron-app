c354f241e14110b930583114f896ad53863eda51
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-04-28 02:39:00 +0500

    added tailwindcss and completed grid view of app

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -2,13 +2,12 @@ import Sidebar from './components/Sidebar'
 
 function App() {
   return (
-    <div className="container">
-      <div className="header">
-        <h1>Electron React Boilerplate</h1>
-        <div>
-          <a href="#">Saad Siddiqui</a>
-        </div>
+    <div className="grid grid-cols-4">
+      <div className="bg-gray-800 h-screen rounded">
         <Sidebar />
+      </div>
+      <div className="col-span-3">
+        <div className="text-xl text-center justify-center items-center flex mt-20">Content</div>
       </div>
     </div>
   )
--- src/renderer/src/components/Sidebar.jsx
+++ src/renderer/src/components/Sidebar.jsx
@@ -1,5 +1,63 @@
+import { useState } from 'react'
+
 const Sidebar = () => {
-  return <div>Sidebar</div>
+  const [active, setActive] = useState(1)
+  const links = [
+    {
+      name: 'Home',
+      id: 1,
+      path: '/'
+    },
+    {
+      name: 'Emails',
+      id: 2,
+      path: '/about'
+    },
+    {
+      name: 'Contacts',
+      id: 3,
+      path: '/contact'
+    }
+  ]
+
+  const handleButton = (id) => {
+    setActive(id)
+  }
+  return (
+    <div>
+      {/* User Details Area */}
+      <div className="flex items-center justify-center mt-4">
+        <div className="flex flex-col items-center">
+          <div className="w-24 h-24 rounded-full bg-gray-200"></div>
+          <div className="mt-2 text-white">User Name</div>
+        </div>
+      </div>
+
+      {/* Sidebar Links */}
+
+      <div className="mt-8">
+        <div className="flex items-left justify-left px-4">
+          <div className="flex flex-col items-center justify-center">
+            <div className="mt-2 items-center justify-center flex-col text-hex-9feaf9">
+              {links.map((link) => (
+                <button
+                  key={link.id}
+                  className={
+                    active === link.id
+                      ? 'flex items-center justify-center  rounded-full mt-10 bg-gray-600 px-4 py-2'
+                      : 'flex items-center justify-center  rounded-full mt-10 bg-gray-800 px-4 py-2'
+                  }
+                  onClick={() => handleButton(link.id)}
+                >
+                  <div className="text-white">{link.name}</div>
+                </button>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Sidebar
